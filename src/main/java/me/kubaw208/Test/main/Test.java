eb6e17514ec2504c36352399e64e8ce0cf9e0289
Jakub <kubaw208kubaw208@gmail.com>
2022-07-08 02:13:03 +0200

    Initial commit

---
+++ src/main/java/me/kubaw208/Test/main/Test.java
@@ -0,0 +1,14 @@
+package me.kubaw208.Test.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Test extends JavaPlugin {
+
+    public static Test plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+    }
+
+}
